459826c142170523c7992abc298b26be026df258
Pamela Heredia <pame.a.heredia@gmail.com>
2021-04-05 20:05:17 -0300

    subiendo cambios en .js

--- src/public/js/pamela-practica1.js
+++ src/public/js/pamela-practica1.js
@@ -11,12 +11,12 @@ function ejercicio6(message, ips = "ipsum"){
 }  
 
 //ej 7
-const a = Math.floor(Math.random() * 5); 
+const a = Math.floor(Math.random() * 5); //const o var? 
 const b = Math.floor(Math.random() * 5);
 const c = Math.floor(Math.random() * 5); 
 
 function ejercicio7(){
-    log("potencia: ("+a+"+"+b+")"+"^"+c+"= "+Math.pow((a+b),c)); 
+    log("potencia: ("+a+"+"+b+")"+"^"+c+" = "+Math.pow((a+b),c)); 
     log("Números: "+a+", "+b+", "+c+". El máximo es: "+Math.max(a,b,c));
 }
 
@@ -24,22 +24,26 @@ function ejercicio7(){
 
 var dia1 = new Date();
 var dia2 = new Date(1575978300000); 
+ 
+//formato dd/mm/aaaa hh:ss.
 
-function imprimirFecha(dia2){
-    log("Fecha: "+dia2); 
+function imprimirFecha(unDia){ 
+    log(unDia.getDay()+"/"+unDia.getMonth()+"/"+unDia.getFullYear()+" "
+    +unDia.getHours()+":"+unDia.getMinutes());  
 }
 
 function recibirFechas(f1, f2){
     f2.setFullYear(f2.getFullYear()); 
-    f1 = f2.getMonth();
+    f1.setMonth(f2.getMonth());
+    imprimirFecha(f2);
     imprimirFecha(f1);
-    imprimirFecha(f2);    
+    
 }
 
 function restarFechas(f1, f2){
-    var nueva = new date (f2-f1); 
-    imprimirFecha(nueva); 
+    const nuevaFecha = Math.abs(f2-f1);
+    const fechaEnDias = Math.ceil(nuevaFecha/(86400000)); //Las fechas son en miliseg = 1000ms * 60m * 60s * 24h
+    log(fechaEnDias); 
 }
 
-//ej 9
 
