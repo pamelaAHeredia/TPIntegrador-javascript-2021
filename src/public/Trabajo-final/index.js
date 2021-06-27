0782954b9dd42752e912a37c5da9aabdc8754b33
Federico Herce <federicoherce25@gmail.com>
2021-06-27 16:58:06 -0300

    backend PPTLS

--- src/public/Trabajo-final/index.js
+++ src/public/Trabajo-final/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
-const ppt = require('./PPTLS/piedraPapelTijera.js')
 const app = express();
 const PORT = 3000;
-
+const ppt = require('./PPTLS/piedraPapelTijera.js')
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -11,8 +10,18 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+app.get('/PPTLS/' , (req,res) => {
+	res.sendFile(path.resolve(__dirname, 'public/PiedraPapelTijera.html'));
 });
+
+
+app.post('/PPTLS/', ppt.crearSala);
+app.patch("/PPTLS/:idSala/", ppt.unirseASala);
+app.post('/PPTLS/:idSala', ppt.guardarMovimiento);
+app.get('/PPTLS/:idSala/:idJugador', ppt.verificarGanador);
+app.patch('/PPTLS/:idSala/:idGanador', ppt.actualizarSala);
+app.delete('/PPTLS/:idSala', ppt.eliminarSala);
+
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
