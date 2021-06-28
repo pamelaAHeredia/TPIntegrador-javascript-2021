e7224ff23375ef7383f91b6ff92e18473e981904
Pamela Heredia <pame.a.heredia@gmail.com>
2021-06-27 22:28:30 -0300

    subiendo ahorcado

--- src/public/Trabajo-final/index.js
+++ src/public/Trabajo-final/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-const ppt = require('./PPTLS/piedraPapelTijera.js')
+const ppt = require('./PPTLS/piedraPapelTijera.js');
+const hm = require('./HangMan.js');
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -15,6 +16,14 @@ app.get('/PPTLS/' , (req,res) => {
 	res.sendFile(path.resolve(__dirname, 'public/PiedraPapelTijera.html'));
 });
 
+//Hangman
+app.get('/HangMan/' , (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public/HangMan/HangMan.html'));
+});
+// Crear una sala
+app.post('/HangMan/salas', hm.crearSala);
+// Jugar una letra
+app.patch('/hangman/salas/:salaId', hm.jugarLetra);
 
 app.post('/PPTLS/', ppt.crearSala);
 app.patch("/PPTLS/:idSala/", ppt.unirseASala);
